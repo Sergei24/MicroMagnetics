100c0527eb9a15c89c2cad962a200fe88f641794
Sergei24 <sergeikozlov24@yandex.ru>
2017-01-28 02:14:45 +0300

    Update H_ef.py

--- H_ef.py
+++ H_ef.py
@@ -32,9 +32,8 @@ def set_n_demag(c, permute, func):
     it.iternext()
   return h_demag
 
-# compute effective field (demag + exchange)
+# demag field
 def h_demag(m):
-  # demag field
   m_pad[:n[0],:n[1],:n[2],:] = m
   f_m_pad = np.fft.rfftn(m_pad, axes = filter(lambda i: n[i] > 1, range(3)))
   f_h_demag_pad = np.zeros(f_m_pad.shape, dtype=f_m_pad.dtype)
@@ -42,11 +41,12 @@ def h_demag(m):
   f_h_demag_pad[:,:,:,1] = (f_n_demag[:,:,:,(1, 3, 4)]*f_m_pad).sum(axis = 3)
   f_h_demag_pad[:,:,:,2] = (f_n_demag[:,:,:,(2, 4, 5)]*f_m_pad).sum(axis = 3)
   h_demag = np.fft.irfftn(f_h_demag_pad, axes = filter(lambda i: n[i] > 1, range(3)))[:n[0],:n[1],:n[2],:]
+  return h_demag
 
-  # exchange field
+# exchange field
 def h_ex(m):  
   h_ex = - 2 * m * sum([1/x**2 for x in dx])
   for i in range(6):
     h_ex += np.repeat(m, 1 if n[i%3] == 1 else [i/3*2] + [1]*(n[i%3]-2) + [2-i/3*2], axis = i%3) / dx[i%3]**2
 
-  return (2*A/mu0)*h_ex
+  return h_ex
